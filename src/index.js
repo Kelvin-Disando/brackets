86b7dbe721f925d3719f44fc31b8c8e8fc5cff3d
Denis Danilkov <kvantokost@gmail.com>
2019-09-25 16:07:05 +0300

    feat: solved task all tests passed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let arrayBrackets = str.split('');
+    let openBracket;
+    let closeBracket;
+    let isSpliced = true;
+
+    while (isSpliced) {
+
+        isSpliced = false;
+
+        for (let i = 0; i < arrayBrackets.length; i++) {
+            const element = arrayBrackets[i];
+            const previousElement = arrayBrackets[i - 1];
+            bracketsConfig.forEach((subarray, i, array) => {
+                if (subarray[1] == element) {
+                    openBracket = subarray[0];
+                    closeBracket = subarray[1];
+                }
+            })
+            if (previousElement == openBracket && element == closeBracket) {
+                arrayBrackets.splice(i - 1, 2)
+                isSpliced = true;
+                break;
+            }
+        }
+    }
+    if (arrayBrackets.length == 0) { return true }
+    else { return false }
+
 }
